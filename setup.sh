43abb3e85f09e93a0f44e85635a4e834f7f95da4
Ben Olson <macslayer@peru3.eecs.utk.edu>
2019-02-22 11:47:03 -0500

    Fixing setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # We need a newer compiler than Debian Stable provides
 . $SPACK_DIR/share/spack/setup-env.sh
 spack install gcc@7.2.0
+spack load gcc@7.2.0
 spack compiler find
