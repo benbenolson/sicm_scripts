e381c5c78b46486c2383f9162b3cd57d259d454c
Michael Jantz <macslayer@peru1.eecs.utk.edu>
2019-02-27 11:07:47 -0500

    Converging on good lulesh size

--- benchmarks/lulesh/lulesh_sizes.sh
+++ benchmarks/lulesh/lulesh_sizes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export SMALL="./lulesh2.0 -s 220 -i 15 -r 11 -b 0 -c 64 -p"
-export LARGE="./lulesh2.0 -s 440 -i 3 -r 11 -b 0 -c 64 -p"
+export LARGE="./lulesh2.0 -s 440 -i 6 -r 11 -b 0 -c 64 -p"
