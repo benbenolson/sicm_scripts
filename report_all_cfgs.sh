bca63fbb49321bf1988a389bc283f2bb0841f13d
Ben Olson <macslayer@peru3.eecs.utk.edu>
2019-04-04 15:20:27 -0400

    Helper scripts

---
+++ report_all_cfgs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CFGS=(pebs_128
+      firsttouch_all_shared_site_0
+      firsttouch_all_exclusive_device_0
+      firsttouch_all_exclusive_device_1
+      offline_all_pebs_guided_128_small_knapsack
+      offline_all_pebs_guided_128_medium_knapsack
+      offline_all_pebs_guided_128_large_knapsack
+      offline_all_pebs_guided_128_small_thermos
+      offline_all_pebs_guided_128_medium_thermos
+      offline_all_pebs_guided_128_large_thermos)
+CFGSTR=""
+for cfg in ${CFGS[@]}; do
+  CFGSTR="${cfg},${CFGSTR}"
+done
+
+./report.sh --cfgs="${CFGSTR}" \
+            --benches=lulesh \
+            --sizes=small,medium,large \
+            --metric=runtime
