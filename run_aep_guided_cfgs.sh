4d5f60b1d5fc10ab91d9b8d58477b0b0abdb5b0f
Michael Jantz <macslayer@peru1.eecs.utk.edu>
2019-05-26 17:26:24 -0400

    Adding small_aep-guided configs

--- run_aep_guided_cfgs.sh
+++ run_aep_guided_cfgs.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 5 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 10 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 15 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 20 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 25 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 30 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 35 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 40 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 45 1
-./run.sh $1 small offline_pebs_guided 128 small_aep hotset 50 1
-
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 5 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 10 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 15 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 20 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 25 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 30 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 35 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 40 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 45 1
-./run.sh $1 small offline_pebs_guided 128 small_aep knapsack 50 1
-
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 5 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 10 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 15 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 20 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 25 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 30 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 35 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 40 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 45 1
-./run.sh $1 small offline_pebs_guided 128 small_aep thermos 50 1
+./run.sh $1 small offline_all_pebs_guided 128 small_aep hotset 1 0
+./run.sh $1 medium offline_all_pebs_guided 128 small_aep hotset 1 0
+./run.sh $1 large offline_all_pebs_guided 128 small_aep hotset 1 0
