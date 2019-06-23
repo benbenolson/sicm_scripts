8370c0e15808f1466b8277b688579c570bd36cb7
Ben Olson <molson5@vols.utk.edu>
2019-06-23 14:52:55 -0700

    About to pull some KNL changes

--- benchmarks/amg/amg_sizes.sh
+++ benchmarks/amg/amg_sizes.sh
@@ -5,6 +5,11 @@ SMALL="./amg -problem 2 -n 120 120 120"
 MEDIUM="./amg -problem 2 -n 220 220 220"
 LARGE="./amg -problem 2 -n 270 270 270"
 
+SMALL_AEP="./amg -problem 2 -n 120 120 120"
+MEDIUM_AEP="./amg -problem 2 -n 400 400 400"
+LARGE_AEP="./amg -problem 2 -n 520 520 520"
+HUGE_AEP="./amg -problem 2 -n 600 600 600"
+
 function amg_medium_pebs_128 {
   export JE_MALLOC_CONF="oversize_threshold:0,background_thread:true,max_background_threads:2"
 }
