226ac517486fca39cc752f3067302008d34679c4
Ben Olson <molson5@vols.utk.edu>
2020-03-12 14:23:29 -0700

    Fixing QMCPACK Spackless build

--- all/bench_build.sh
+++ all/bench_build.sh
@@ -6,28 +6,28 @@
 function bench_build {
   # Use Spack to load SICM into the environment
   if [ "$1" = "fort" ]; then
-    export LD_LINKER="flang $2 -Wno-unused-command-line-argument -Wl,-rpath,${SICM_PREFIX}/lib -L${SICM_PREFIX}/lib -lflang -lflangrti"
+    export LD_LINKER="flang $2 -Wno-unused-command-line-argument -Wl,-rpath,${SICM_PREFIX}/lib -L${SICM_PREFIX}/lib -lflang -lflangrti -g"
   elif [ "$1" = "c" ]; then
-    export LD_LINKER="clang++ $2 -Wno-unused-command-line-argument -L${SICM_PREFIX}/lib -lflang -lflangrti -Wl,-rpath,${SICM_PREFIX}/lib"
+    export LD_LINKER="clang++ $2 -Wno-unused-command-line-argument -L${SICM_PREFIX}/lib -lflang -lflangrti -Wl,-rpath,${SICM_PREFIX}/lib -g"
   else
     echo "No linker specified. Aborting."
     exit
   fi
 
   # Define the variables for the compiler wrappers
-  export LD_COMPILER="clang++ -Wno-unused-command-line-argument -march=x86-64" # Compiles from .bc -> .o
-  export CXX_COMPILER="clang++ $3  -Wno-unused-command-line-argument -march=x86-64"
-  export FORT_COMPILER="flang $3  -Mpreprocess -Wno-unused-command-line-argument -march=x86-64 -I${SICM_PREFIX}/include -L${SICM_PREFIX}/lib -lflang -lflangrti"
-  export C_COMPILER="clang  $3 -Wno-unused-command-line-argument -march=x86-64"
+  export LD_COMPILER="clang++ -Wno-unused-command-line-argument -march=x86-64 -g" # Compiles from .bc -> .o
+  export CXX_COMPILER="clang++ $3  -Wno-unused-command-line-argument -march=x86-64 -g"
+  export FORT_COMPILER="flang $3  -Mpreprocess -Wno-unused-command-line-argument -march=x86-64 -I${SICM_PREFIX}/include -L${SICM_PREFIX}/lib -lflang -lflangrti -g"
+  export C_COMPILER="clang  $3 -Wno-unused-command-line-argument -march=x86-64 -g"
   export LLVMLINK="llvm-link"
   export LLVMOPT="opt"
 
   # Make sure the Makefiles find our wrappers
   export COMPILER_WRAPPER="compiler_wrapper.sh "
   export LD_WRAPPER="ld_wrapper.sh "
-  export PREPROCESS_WRAPPER="clang -E -x c -P"
-  export AR_WRAPPER="ar_wrapper.sh"
-  export RANLIB_WRAPPER="ranlib_wrapper.sh"
+  export PREPROCESS_WRAPPER="clang -E -x c -P "
+  export AR_WRAPPER="ar_wrapper.sh "
+  export RANLIB_WRAPPER="ranlib_wrapper.sh "
 }
 
 function bench_build_no_transform {
