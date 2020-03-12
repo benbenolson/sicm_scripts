2fc30d1f170eae4b54dd8525a9c4edcb02e077dc
Ben Olson <molson5@vols.utk.edu>
2020-03-12 11:39:18 -0700

    Updating build system to not use Spack

---
+++ bench_build_nospack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+# First argument is the benchmark name
+
+source ./all/vars.sh
+source ${SCRIPTS_DIR}/all/args_nospack.sh
+source ${SCRIPTS_DIR}/all/bench_build.sh
+
+if [[ ${#BENCHES[@]} = 0 ]]; then
+  echo "You didn't specify a benchmark name. Aborting."
+  exit 1
+fi
+
+export PATH="${SICM_PREFIX}/bin:${PATH}"
+export LD_LIBRARY_PATH="${SICM_PREFIX}/lib:${LD_LIBRARY_PATH}"
+export LIBRARY_PATH="${SICM_PREFIX}/lib:${LIBRARY_PATH}"
+export CPATH="${SICM_PREFIX}/include:${CPATH}"
+export CMAKE_PREFIX_PATH="${SICM_PREFIX}/:${CMAKE_PREFIX_PATH}"
+
+export FC="${SICM_PREFIX}/bin/flang"
+export F77="${SICM_PREFIX}/bin/flang"
+export F90="${SICM_PREFIX}/bin/flang"
+
+for BENCH in ${BENCHES[@]}; do
+
+  echo "Compiling ${BENCH} with $SICM."
+
+  # Clean up the source directory first
+  cd $BENCH_DIR/${BENCH}/src
+  rm *.o *.bc *.args *.mod contexts.txt nclones.txt nsites.txt buCG.txt .sicm_ir.bc
+
+  ${SCRIPTS_DIR}/benchmarks/${BENCH}/${BENCH}_build.sh
+done
